a7d33f7abba729d4645724ee31b17fd98833e02c
JZhang <2j.zy.zhang@gmail.com>
2016-03-29 23:55:38 -0400

    minor fix

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
         $(index_img).attr("src","./images/2.jpg");
     });
     $("#selection").change(function(){
-    	alert($(this).val());
     	window.location = $(this).val();
     });
 });
