7780ba18487af081319e6368805348b7c7c6c03c
JZhang <2j.zy.zhang@gmail.com>
2016-03-30 22:57:15 -0400

    index page, js
    index page image randomized and added more images. js added accordingly.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
+
+function rand_num(max){
+	var x = Math.floor((Math.random()*max) + 1);
+	return x;
+}
+
 $(document).ready(function(){
+    //randomly select picture
     $("#button").click(function(){
-        $(index_img).attr("src","./images/2.jpg");
+    	var imgnum = rand_num(10);
+        $("#index_img").attr("src","./images/"+imgnum+".jpg");	
     });
     $("#selection").change(function(){
     	window.location = $(this).val();
